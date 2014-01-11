0074b72f82ddf1c1679320b288eb02522de8ca2a
Sascha Grossenbacher <saschagros@gmail.com>
2014-01-11 12:49:13 +0100

    Issue #2168681 by blueminds, Berdir, miro_dietiker: Fixed Enable module sensor is critical after accepting defaults due to profile and hidden modules.

--- lib/Drupal/monitoring/Sensor/Sensors/SensorEnabledModules.php
+++ lib/Drupal/monitoring/Sensor/Sensors/SensorEnabledModules.php
@@ -31,36 +31,46 @@ class SensorEnabledModules extends SensorConfigurable {
     );
 
     // Get current list of modules.
-    $files = system_rebuild_module_data();
+    $modules = system_rebuild_module_data();
+    $visible_modules = array();
+    $hidden_modules = array();
 
-    // Remove hidden modules from display list.
-    $visible_files = $files;
-    foreach ($visible_files as $filename => $file) {
-      if (!empty($file->info['hidden'])) {
-        unset($visible_files[$filename]);
+    foreach ($modules as $module => $module_data) {
+      // Skip profiles.
+      if (strpos(drupal_get_path('module', $module), 'profiles') === 0) {
+        continue;
+      }
+      // As we also include hidden modules, some might have no name at all,
+      // make sure it is set.
+      if (!isset($module_data->info['name'])) {
+        $module_data->info['name'] = '- No name -';
+      }
+      if (!empty($module_data->info['hidden'])) {
+        $module_data->info['name'] .= ' [' . t('Hidden module') .']';
+        $hidden_modules[$module] = $module_data;
+      }
+      else {
+        $visible_modules[$module] = $module_data;
       }
     }
 
-    uasort($visible_files, 'system_sort_modules_by_info_name');
-
-    $modules = array();
-    foreach ($visible_files as $module) {
-      $modules[$module->name] = $module->info['name'];
-    }
+    uasort($visible_modules, 'system_sort_modules_by_info_name');
+    uasort($hidden_modules, 'system_sort_modules_by_info_name');
 
     $default_value = $this->info->getSetting('modules');
 
     if (empty($default_value)) {
       $default_value = module_list();
-      $form['preselect_info'] = array(
-        '#type' => 'markup',
-        '#markup' => '<div class="messages warning">' . t('Note that preselected modules are not actual settings of this sensor. To have the preselection become the settings you need to confirm the selection by saving the form values.') . '</div>',
-      );
+    }
+
+    $options = array();
+    foreach (array_merge($visible_modules, $hidden_modules) as $module => $module_data) {
+      $options[$module] = $module_data->info['name'] . ' (' . $module . ')';
     }
 
     $form['modules'] = array(
       '#type' => 'checkboxes',
-      '#options' => $modules,
+      '#options' => $options,
       '#title' => t('Modules expected to be enabled'),
       '#description' => t('Check all modules that are supposed to be enabled.'),
       '#default_value' => $default_value,
@@ -77,8 +87,25 @@ class SensorEnabledModules extends SensorConfigurable {
     $result->setSensorExpectedValue(0);
     $delta = 0;
 
-    $enabled_modules = module_list();
+    $enabled_modules = array();
+    // Filter out install profile.
+    foreach (module_list() as $module) {
+      $path_parts = explode('/', drupal_get_path('module', $module));
+      if ($path_parts[0] != 'profiles') {
+        $enabled_modules[$module] = $module;
+      }
+    }
+
     $expected_modules = array_filter($this->info->getSetting('modules'));
+
+    // If there are no expected modules, the sensor is not configured, so init
+    // the expected modules list as currently enabled modules.
+    if (empty($expected_modules)) {
+      $expected_modules = $enabled_modules;
+      $settings = monitoring_sensor_settings_get('enabled_modules');
+      $settings['modules'] = $enabled_modules;
+      monitoring_sensor_settings_save('enabled_modules', $settings);
+    }
 
     // Check for modules not being installed but expected.
     $non_installed_modules = array_diff($expected_modules, $enabled_modules);
