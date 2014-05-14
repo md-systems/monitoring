fe20c42a2edb189ef5153e8f985d5d7d2ce97407
Karan Poddar <karan.poddar@gmail.com>
2014-05-15 00:29:14 +0530

    Adding Annotation Class

---
+++ lib/Drupal/monitoring/Annotation/Sensor.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\monitoring\Annotation\Sensor.
+ */
+
+namespace Drupal\monitoring\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines Sensor annotation object which will be
+ * for reference by Sensor Plugins.
+ *
+ * @Annotation
+ */
+class Sensor extends Plugin {
+  
+   /**
+   * The plugin ID.
+   *
+   * @var string
+   */
+  public $id;
+
+}
