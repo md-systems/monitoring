fab807f1ef15aade2d9e9ae21ed90ef382a4e10e
Karan Poddar <karan.poddar@gmail.com>
2014-05-14 19:43:21 +0530

    Made changes to Sensor class to support Plugins

--- lib/Drupal/monitoring/Sensor/Sensor.php
+++ lib/Drupal/monitoring/Sensor/Sensor.php
@@ -6,12 +6,17 @@
 
 namespace Drupal\monitoring\Sensor;
 
+use Drupal\Core\Plugin\PluginInspectionInterface;
+
 /**
  * Abstract SensorInterface implementation with common behaviour.
  *
+ * This abstract class extends PluginInspectionInterface and will be extended by
+ * monitoring plugins.
+ *
  * @todo more
  */
-abstract class Sensor implements SensorInterface {
+abstract class Sensor extends PluginInspectionInterface implements SensorInterface {
 
   /**
    * Current sensor info object.
@@ -22,12 +27,32 @@ abstract class Sensor implements SensorInterface {
   protected $services = array();
 
   /**
+   * The plugin_id.
+   *
+   * @var string
+   */
+  protected $pluginId;
+  
+  /**
+   * The plugin implementation definition.
+   *
+   * @var array
+   */
+  protected $pluginDefinition;
+
+  /**
    * Instantiates a sensor object.
    *
    * @param SensorInfo $info
    *   Sensor info object.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
    */
-  function __construct(SensorInfo $info) {
+  function __construct(SensorInfo $info, $plugin_id, $plugin_definition) {
+    $this->pluginId = $plugin_id;
+    $this->pluginDefinition = $plugin_definition;
     $this->info = $info;
   }
 
@@ -61,4 +86,17 @@ abstract class Sensor implements SensorInterface {
     return (boolean) $this->info->getSetting('enabled');
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginId() {
+    return $this->pluginId;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginDefinition() {
+    return $this->pluginDefinition;
+  }
 }
