bebd112671691a8feedc45ad9c45c9100eb324dd
Karan Poddar <karan.poddar@gmail.com>
2014-05-20 18:19:27 +0530

    Conversion of SensorQueue sensor to Plugin.

--- lib/Drupal/monitoring/Plugin/monitoring/Sensor/SensorQueue.php
+++ lib/Drupal/monitoring/Plugin/monitoring/Sensor/SensorQueue.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @file
- * Contains \Drupal\monitoring\Sensor\Sensors\SensorQueue.
+ * Contains \Drupal\monitoring\Plugin\monitoring\Sensor\SensorQueue.
  */
 
-namespace Drupal\monitoring\Sensor\Sensors;
+namespace Drupal\monitoring\Plugin\monitoring\Sensor;
 
 use Drupal\monitoring\Result\SensorResultInterface;
 use Drupal\monitoring\Sensor\SensorThresholds;
@@ -12,6 +12,12 @@ use Drupal;
 
 /**
  * Monitors number of items for a given core queue.
+ *
+ * @Sensor(
+ *   id = "monitoring_queue",
+ *   label = @Translation("Queue"),
+ *   description = @Translation("Monitors number of items for a given core queue.")
+ * )
  *
  * Every instance represents a single queue.
  * Once all queue items are processed, the value should be 0.
