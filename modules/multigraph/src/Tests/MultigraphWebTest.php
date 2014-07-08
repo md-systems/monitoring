0006028bd608e28d3e449dfbdb17a2fb0f5f971d
James Cazzetta <james.cazzetta@gmail.com>
2014-07-08 13:25:23 +0200

    Added tests for creating multigraph sensor bundles.

--- modules/multigraph/src/Tests/MultigraphWebTest.php
+++ modules/multigraph/src/Tests/MultigraphWebTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Contains \MultigraphSimpleTest
+ * Contains Drupal\monitoring_multigraph\Tests\MultigraphSimpleTest
  */
 
 namespace Drupal\monitoring_multigraph\Tests;
@@ -71,12 +71,35 @@ class MultigraphWebTest extends WebTestBase {
    * Flag creation.
    */
   public function doTestMultigraphAdd() {
-    $edit = array(
+    // Add a few sensors.
+    $edit0 = array(
       'label' => $this->label,
       'id' => $this->id,
       'description' => $this->description,
+      'sensor_add_select' => 'dblog_404',
     );
-    $this->drupalPostForm('admin/config/system/monitoring/multigraphs/add', $edit, t('Save'));
+    $this->drupalPostForm('admin/config/system/monitoring/multigraphs/add', $edit0, t('Add sensor'));
+    $this->assertText(t('Page not found errors'));
+
+    $this->drupalPostForm('admin/config/system/monitoring/multigraphs/add', array(
+      'sensor_add_select' => 'node_new_page',
+    ), t('Add sensor'));
+    $this->assertText(t('New Basic page nodes'));
+
+    $this->drupalPostForm('admin/config/system/monitoring/multigraphs/add', array(
+      'sensor_add_select' => 'user_successful_logins',
+    ), t('Add sensor'));
+    $this->assertText(t('Successful user logins'));
+
+    // And last but not least, change all sensor label values and save form.
+    $this->drupalPostForm('admin/config/system/monitoring/multigraphs/add', array(
+      'sensors[dblog_404][label][data]' => 'Page not found errors (test)',
+      'sensors[node_new_article][label][data]' => 'New Article nodes (test)',
+      'sensors[user_successful_logins][label][data]' => 'Successful user logins (test)',
+    ), t('Save'));
+    $this->assertText(t('Multigraph settings saved.'));
+    $this->assertText(t('New Article nodes (test), Page not found errors (test), Successful user logins (test)'));
+
     // Check for fieldset titles.
     $this->assertText(t('Add Multigraph'));
   }
